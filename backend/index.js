8dcbff61960235e72776a548ece0c610b6eebf38
Morax25 <adarsh@infoneotech.com>
2026-04-02 14:29:39 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,39 @@
-import http from "http"
-import { Server } from "socket.io"
-import express from "express"
-import dotenv from "dotenv"
-import { pool, testConnection } from "./src/database/index.js"
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { pool } from "./src/database/index.js";
 
-
-dotenv.config()
-
-const users = await pool.query('SELECT * FROM users')
-console.log(users.rows.map((user) => ({ name: user.username, email:user.email })))
-
+dotenv.config();
+const users = await pool.query("SELECT * FROM users");
+console.log(
+  users.rows.map((user) => ({ name: user.username, email: user.email })),
+);
 try {
-    const user = pool.query(`
-     INSERT INTO users (username, email, password) VALUES ('ravi${Math.ceil((Math.random() * 100)) + 1}', 'ravi${Math.ceil((Math.random() * 100)) + 1}@gmail.com', 'ravi@1234');
-    `)
+  const user = pool.query(`
+     INSERT INTO users (username, email, password) VALUES ('ravi${Math.ceil(Math.random() * 100) + 1}', 'ravi${Math.ceil(Math.random() * 100) + 1}@gmail.com', 'ravi@1234');
+    `);
 } catch (error) {
-    console.log(error.message)
+  console.log(error.message);
 }
-
-
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-})
-app.get('/', (req, res) => {
-    res.send("<h1>Hi i am a server element<h1/>")
-})
-io.on('connection', (socket) => {
-    console.log(socket.id)
-    socket.on('message', (message) => {
-        console.log(message)
-        socket.broadcast.emit("received_message", message)
-    })
-})
+  cors: {
+    origin: "*",
+  },
+});
+app.get("/", (req, res) => {
+  res.send("<h1>Hi i am a server element<h1/>");
+});
+const chatRoomUser = new Map();
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (message) => {
+    console.log(message);
+    socket.broadcast.emit("received_message", message);
+  });
+});
 server.listen(3001, () => {
-    console.log("The server is listening on PORT : 3001")
-})
+  console.log("The server is listening on PORT : 3001");
+});
--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,59 +1,61 @@
-import dotenv from 'dotenv'
-import pkg from 'pg'
-import pRetry from 'p-retry'
+import dotenv from "dotenv";
+import pRetry from "p-retry";
+import pkg from "pg";
 
-dotenv.config()
+dotenv.config();
 
 const { Pool } = pkg;
 
 const {
-    PG_HOST,
-    PG_PORT,
-    PG_USER,
-    PG_PASSWORD,
-    PG_DATABASE,
-    PG_MAX_CLIENTS,
-    PG_IDLE_TIMEOUT,
-    PG_CONNECTION_TIMEOUT
-} = process.env
+  PG_HOST,
+  PG_PORT,
+  PG_USER,
+  PG_PASSWORD,
+  PG_DATABASE,
+  PG_MAX_CLIENTS,
+  PG_IDLE_TIMEOUT,
+  PG_CONNECTION_TIMEOUT,
+} = process.env;
 
 export const pool = new Pool({
-    host: PG_HOST,
-    port: PG_PORT,
-    user: PG_USER,
-    password: PG_PASSWORD,
-    database: PG_DATABASE,
-    max: Number(PG_MAX_CLIENTS),
-    idleTimeoutMillis: Number(PG_IDLE_TIMEOUT),
-    connectionTimeoutMillis: Number(PG_CONNECTION_TIMEOUT)
-})
+  host: PG_HOST,
+  port: PG_PORT,
+  user: PG_USER,
+  password: PG_PASSWORD,
+  database: PG_DATABASE,
+  max: Number(PG_MAX_CLIENTS),
+  idleTimeoutMillis: Number(PG_IDLE_TIMEOUT),
+  connectionTimeoutMillis: Number(PG_CONNECTION_TIMEOUT),
+});
 
 export async function testConnection() {
-    await pRetry(
-        async () => {
-            const client = await pool.connect()
-            try {
-                await pool.query('SELECT 1')
-                console.log("Post gres connection established")
-            } catch (error) {
-                client.release()
-            }
-        },
-        {
-            retries: 10,
-            minTimeout: 2000,
-            onFailedAttempt: (err) => {
-                console.warn(`PG Connection attempt ${err.attemptNumber} failed. ${err.retriesLeft} retries left`, err.error.message)
-            }
-        }
-
-    )
+  await pRetry(
+    async () => {
+      const client = await pool.connect();
+      try {
+        await pool.query("SELECT 1");
+        console.log("PostGres connection established");
+      } catch (error) {
+        client.release();
+      }
+    },
+    {
+      retries: 10,
+      minTimeout: 2000,
+      onFailedAttempt: (err) => {
+        console.warn(
+          `PG Connection attempt ${err.attemptNumber} failed. ${err.retriesLeft} retries left`,
+          err.error.message,
+        );
+      },
+    },
+  );
 }
 
-await testConnection()
+await testConnection();
 
 const closePool = () => {
-    pool.end().then(() => {
-        console.log("postgres pool closed")
-    })
-}
+  pool.end().then(() => {
+    console.log("postgres pool closed");
+  });
+};
